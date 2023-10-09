9043d6defd1efa91af52db303fffec5022666d52
Jari Maijenburg <jarimayenburg@hotmail.com>
2023-10-09 22:52:47 +0200

    Replaced netrw with neo-tree when opening a directory

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
+  lazy = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
@@ -52,12 +53,13 @@ return {
           ["on"] = false,
           ["os"] = false,
           ["ot"] = false,
-        }
+        },
       },
       filtered_items = {
         show_hidden_count = false
       },
       group_empty_dirs = true,
+      hijack_netrw_behavior = "open_current",
     }
   }
 }
